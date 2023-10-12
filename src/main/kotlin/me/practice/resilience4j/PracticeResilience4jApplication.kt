ced14bfd3b071fffa1f980f4fa4c696872f85fad
jimmy.lee <dongeol92@daum.net>
2023-10-12 18:28:09 +0900

    init

---
+++ src/main/kotlin/me/practice/resilience4j/PracticeResilience4jApplication.kt
@@ -0,0 +1,11 @@
+package me.practice.resilience4j
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeResilience4jApplication
+
+fun main(args: Array<String>) {
+    runApplication<PracticeResilience4jApplication>(*args)
+}
---
+++ src/test/kotlin/me/practice/resilience4j/PracticeResilience4jApplicationTests.kt
@@ -0,0 +1,12 @@
+package me.practice.resilience4j
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeResilience4jApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
